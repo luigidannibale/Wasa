51212a889c1d500aa27c4751065ff32e0e91bf25
luigidannibale <dannibale.1991254@studenti.uniroma1.it>
2023-11-21 20:50:47 +0100

    Update put-followUser.go
    Commented to work on it in the future

--- service/api/put-followUser.go
+++ service/api/put-followUser.go
@@ -1,39 +1,35 @@
 package api
 
 import (
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"encoding/json"
-	"strconv"
-	"io"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-
-func (rt *_router) folloUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (rt *_router) followUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("content-type", "application/json")
-	
-	//body, _ := io.readAll(r.Body)	
-	//This should get the body
-	var user User
-	json.Unmarshal(r.Body, &user)
-	
-	//This takes the userID from parameters
-	userID, err := strconv.Atoi(ps.ByName("userID"))
+	/*
+		//body, _ := io.readAll(r.Body)
+		//This should get the body
+		var user User
+		json.Unmarshal(r.Body, &user)
 
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	if Users[userID] == nil {
-		w.WriteHeader(http.StatusNotFound)
-		return userID
-	}
-	if Users[user.id] == nil {
-		w.WriteHeader(http.StatusNotFound)
-		return body
-	}
-	// Insert the user into the followed 		
-	// Users[userID].followed.append(user)
+		//This takes the userID from parameters
+		userID, err := strconv.Atoi(ps.ByName("userID"))
 
-
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if Users[userID] == nil {
+			w.WriteHeader(http.StatusNotFound)
+			return userID
+		}
+		if Users[user.id] == nil {
+			w.WriteHeader(http.StatusNotFound)
+			return body
+		}
+		// Insert the user into the followed
+		// Users[userID].followed.append(user)
+	*/
 }
