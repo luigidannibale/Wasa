555fe69eca1a0ece71e082c64a5574f54953696d
luigidannibale <dannibale.1991254@studenti.uniroma1.it>
2023-11-21 19:41:02 +0100

    Loaded some fantastic coffee structure
    Added some methods to database, and api

--- service/api/api.go
+++ service/api/api.go
@@ -38,10 +38,11 @@ package api
 
 import (
 	"errors"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
-	"github.com/julienschmidt/httprouter"
-	"github.com/sirupsen/logrus"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/luigidannibale/Wasa/service/database"
+	"github.com/sirupsen/logrus"
 )
 
 // Config is used to provide dependencies and configuration to the New function.
---
+++ service/database/unfollow.go
@@ -0,0 +1,11 @@
+package database
+
+//import "errors"
+
+func (db *appdbimpl) Unfollow(userID int, userToUnfollow int) (string, error) {
+
+	// return "Could't find the user", errors.New("UserNotFound")
+	// return "Could't find the user to unfollow", errors.New("FollowedNotFound")
+	// return "An error occured on the server", errors.New("InternalServerError")
+	return "User unfollowed successfully", nil
+}
